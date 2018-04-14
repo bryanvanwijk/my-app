4085e286bed8745cf83afc74e037ac26ee56bce0
Bryan van Wijk <bryanvanwijk@gmail.com>
2018-04-15 00:54:00 +0200

    Add load data

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IState {
+  data: string;
+}
+
+class App extends React.Component<{}, IState> {
+
+  public componentDidMount() {
+    this.loadData();
+  }
+
+  public async loadData() {
+    const res = await fetch("/api/test");
+    const data  = await res.json();
+    this.setState({ data })
+  }
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React {this.state.data}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
